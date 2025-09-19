497b4ceea7b515cbdb25e97ec9299aef5e96e22f
phea <sokleapheaphon@gmail.com>
2025-09-19 23:38:41 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { Router } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar/>
+    </Router>
+  )
+}
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import { use, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    return (
+        <nav className="bg-white shadow-md p-5 flex justify-center">
+            <div className="font-bold">
+                <ul className="hidden md:flex space-x-6 items-center">
+                    <li><Link to="/about" className="text-gray-600 hover:text-gray-900">About</Link></li>
+                    <li><Link to="/contact" className="text-gray-600 hover:text-gray-900">Contact</Link></li>
+                    <li><Link to="/project" className="text-gray-600 hover:text-gray-900">Project</Link></li>
+                    <li><Link to="/skill" className="text-gray-600 hover:text-gray-900">Skills</Link></li>
+                </ul>
+            </div>
+            <div className="md:hidden">
+                <button onClick={() => setIsOpen(!isOpen)}>
+                    {isOpen}
+                </button>
+            </div>
+        </nav>
+    )
+};
+
+export default Navbar;
