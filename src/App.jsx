321feaaf6d2383e3e32fd0ad3eb90618f0c0208c
phea <sokleapheaphon@gmail.com>
2025-09-20 21:43:21 +0700

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-// import { Router } from "react-router-dom";
+// App.jsx
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+// import About from "./components/About";
+// import Projects from "./components/Project";
+// import Skills from "./components/Skill";
+// import Contact from "./components/Contact";
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Navbar/>
-    </Router>
-  )
+    <BrowserRouter>
+      <div className="bg-[#6930c3] min-h-screen">
+          <Navbar/>
+          <Routes>
+            {/* <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
+
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Photo from "../assets/photo"
+
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <h1>About Me</h1>
+                <Photo/>
+            </div>
+        </div>
+    )
+}
+export default About;
